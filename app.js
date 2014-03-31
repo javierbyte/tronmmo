1080df8328a52827da49c90cc5de9ee04646d3d8
Javier Bórquez <hola@javierbyte.com>
2014-03-31 02:45:50 -0700

    working

--- app.js
+++ app.js
@@ -12,12 +12,77 @@ app.get('/', function (req, res) {
 });
 app.use(express.static(__dirname + '/public'));
 
-io.sockets.on('connection', function (socket) {
+/* GAME logic */
+var arenaW = 140; //the width of the arena
+var arenaH = 80; //the height of the arena
 
+var a = []; // initializing the arena matrix
+for(x=0;x<arenaW;x++) {
+	a[x] = [];
+	for(y=0;y<arenaH;y++) {
+		a[x][y] = -1;
+	}
+}
+
+var tron = [];
+function createTron(name, id) {
+	this.name = name;
+	this.id = id;
+	this.x = (Math.random()*arenaW)|0;
+	this.y = (Math.random()*arenaH)|0;
+	this.color = "#"+((1<<24)*Math.random()|0).toString(16);
+	this.dir = 1+(Math.random()*4)|0;
+
+	while(a[this.x][this.y] != -1) {
+		this.x = (Math.random()*arenaW)|0;
+		this.y = (Math.random()*arenaH)|0;
+	}
+
+	a[this.x][this.y] = id;
+}
+
+function newTron(name) {
+	tron[tron.length] = new createTron(name, tron.length);
+}
+
+function update() {
+	for(x=0;x<tron.length;x++) {
+		if(tron[x].dir == 1) tron[x].y = tron[x].y - 1;
+		else if(tron[x].dir == 2) tron[x].x = tron[x].x + 1;
+		else if(tron[x].dir == 3) tron[x].y = tron[x].y + 1;
+		else if(tron[x].dir == 4) tron[x].x = tron[x].x - 1;
+
+		if(tron[x].x>=0 && tron[x].x<arenaW && tron[x].y>=0 && tron[x].y<arenaH && a[tron[x].x][tron[x].y] == -1) {
+			a[tron[x].x][tron[x].y] = x;
+		} else {
+			for(i=0;i<arenaW;i++) for(j=0;j<arenaH;j++) if(a[i][j] == x) {
+				a[i][j] = -1;
+			}
+			tron[x] = new createTron(tron[x].name, tron[x].id); //reviving!
+		}
+	}
+}
+
+io.sockets.on('connection', function (socket) {
 	socket.emit('start');
 
-	socket.on('touch', function (data) {
-		socket.broadcast.emit('touch',data);
+	socket.on('newTron', function (data) {
+    	socket.set('tronNumber', tron.length, function () {
+      		socket.emit('msj', 'Welcome, ' + tron.length);
+    	});
+    	newTron(data);
 	});
 
+  	socket.on('move', function (move) {
+    	socket.get('tronNumber', function (err, tronNumber) {
+      		socket.emit('msj', 'move ' + move + ' by ' + tronNumber);
+      		tron[tronNumber].dir = move;
+    	});
+  	});
+
+	setInterval(function() {
+		update();
+		socket.emit('update', a);
+		socket.emit('updateTron', tron);
+	}, 80);
 });
--- public/code.js
+++ public/code.js
@@ -1,29 +1,20 @@
 var socket = io.connect(document.URL);
 
-socket.on('start', function () {
-	console.log('start');
+socket.on('start', function (data) {
+	console.log('starting tron ');
+	socket.emit('newTron', 'dummy');
 });
 
-socket.on('touch', function (data) {
-	if(data) document.getElementById('body').style.background ="#a00";
-	else document.getElementById('body').style.background ="#fff";
+socket.on('msj', function (data){
+	console.log(data);
 });
-
-function touching(n) {
-	socket.emit('touch', n);
-}
 
 /*caching jquery*/
 var $main = $('#main');
 
-function main() {
-	update();
-	render();
-}
-
 /*model*/
-var arenaW = 160; //the width of the arena
-var arenaH = 90; //the height of the arena
+var arenaW = 140; //the width of the arena
+var arenaH = 80; //the height of the arena
 
 var a = []; // initializing the arena matrix
 for(x=0;x<arenaW;x++) {
@@ -33,55 +24,14 @@ for(x=0;x<arenaW;x++) {
 	}
 }
 
+socket.on('update', function (data) {
+	a = data;
+});
+
 var tron = [];
-function createTron(name, id) {
-	this.name = name;
-	this.id = id;
-	this.posx = (Math.random()*arenaW)|0;
-	this.posy = (Math.random()*arenaH)|0;
-	this.color = "#"+((1<<24)*Math.random()|0).toString(16);
-	this.dir = 1+(Math.random()*4)|0;
-
-	while(a[this.posx][this.posy] != -1) {
-		this.posx = (Math.random()*arenaW)|0;
-		this.posy = (Math.random()*arenaH)|0;
-	}
-
-	a[this.posx][this.posy] = id;
-	this.live = 1;
-}
-
-function newTron(name) {
-	tron[tron.length] = new createTron(name, tron.length);
-}
-
-newTron('javier');
-
-
-function update() {
-	now = Date.now();
-	diff = (now - last)/1000;
-	last = now;
-
-	for(x=0;x<tron.length;x++) {
-		if(tron[x].live) {
-			if(tron[x].dir == 1) tron[x].posy = tron[x].posy - 1;
-			else if(tron[x].dir == 2) tron[x].posx = tron[x].posx + 1;
-			else if(tron[x].dir == 3) tron[x].posy = tron[x].posy + 1;
-			else if(tron[x].dir == 4) tron[x].posx = tron[x].posx - 1;
-	
-			if(tron[x].posx>=0 && tron[x].posx<arenaW && tron[x].posy>=0 && tron[x].posy<arenaH && a[tron[x].posx][tron[x].posy] == -1) {
-				a[tron[x].posx][tron[x].posy] = x;
-			} else {
-				tron[x].live = 0;
-				for(i=0;i<arenaW;i++) for(j=0;j<arenaH;j++) if(a[i][j] == x) {
-					a[i][j] = -1;
-					ctx.clearRect(i,j,1,1);
-				}
-			}
-		}		
-	}
-}
+socket.on('updateTron', function (data) {
+	tron = data;
+});
 
 /*view*/
 function render() {
@@ -89,6 +39,8 @@ function render() {
 		if(a[x][y] != -1) {
 			ctx.fillStyle = tron[a[x][y]].color;
 			ctx.fillRect(x,y,1,1);
+		} else {
+			ctx.clearRect(x,y,1,1);
 		}
 	}
 }
@@ -113,5 +65,11 @@ var ctx = c.getContext("2d");
 ctx.scale(ratio, ratio);
 
 /* 3,2,1.. go!*/
-last = Date.now();
-setInterval(main, 50);
+setInterval(render, 1);
+
+$(document).keydown(function(e) {
+    if(e.keyCode == 38) socket.emit('move', 1);
+    else if(e.keyCode == 39) socket.emit('move', 2);
+    else if(e.keyCode == 40) socket.emit('move', 3);
+    else if(e.keyCode == 37) socket.emit('move', 4);
+});
